ad515147b74d7b84b8c9cd60767ecb582cf9be75
yusuke-2310 <shirou0132@gmail.com>
2026-08-24 00:58:29 +0900

    【yusuke】Launcher更新

--- MusicBot_Streaming/src/main/java/com/discord/MusicBot/MusicBotLauncher.java
+++ MusicBot_Streaming/src/main/java/com/discord/MusicBot/MusicBotLauncher.java
@@ -9,7 +9,7 @@ public class MusicBotLauncher {
 	
     FileRepository fileRepository = new FileRepository("music/");
     AudioService audioService = new AudioService(fileRepository);
-//    AudioController controller = new AudioController(audioService);
+    AudioController controller = new AudioController(audioService, fileRepository);
 	
 	private static JDA jda = null;
 	// botのトークン情報を設定
