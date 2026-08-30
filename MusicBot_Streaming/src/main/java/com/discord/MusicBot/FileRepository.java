7a1e1c69f169010bee0ba3ec840899ac39fe7d51
ShirouKashiwagi <shirou0132@gmail.com>
2026-08-30 11:28:43 +0900

    【yusuke】FileRepositoeyクラスにコメント追加

--- MusicBot_Streaming/src/main/java/com/discord/MusicBot/FileRepository.java
+++ MusicBot_Streaming/src/main/java/com/discord/MusicBot/FileRepository.java
@@ -12,12 +12,18 @@ public class FileRepository {
         this.musicFolderPath = musicFolderPath;
     }
 
+    // 音楽全件検索
     public List<String> findAllAudioFiles() {
+    	
+    	// フォルダ変数の定義 
         File folder = new File(musicFolderPath);
+        // ファイルパス先の.mp3を全件検索
         File[] files = folder.listFiles((dir, name) -> name.endsWith(".mp3"));
 
+        // 検索結果がnull以外の場合 - nullの場合空を返却
         if (files == null) return List.of();
 
+        // File[]をリストに変換して返却
         return Arrays.stream(files)
                 .map(File::getName)
                 .toList();
